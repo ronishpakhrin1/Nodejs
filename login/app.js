4e34a3c65d641486b42f80067c468e1322bd8cbe
ronishpakhrin1 <ronishpakhrin@gmail.com>
2019-07-30 20:46:08 +0530

    Removed login directory

--- login/app.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app=express();
-app.use('/',require('./routes/main'));
-app.use('/main2',require('./routes/main2'));
-
-const PORT = process.env.PORT || 8080;
-app.listen(PORT,console.log(`server listening on ${PORT}`));

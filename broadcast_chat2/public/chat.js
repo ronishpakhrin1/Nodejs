caff7a77a0d87dde1718aea38abe6b7e478c6a56
ronishpakhrin1 <ronishpakhrin@gmail.com>
2019-08-01 20:36:51 +0530

    added

---
+++ broadcast_chat2/public/chat.js
@@ -0,0 +1,52 @@
+var socket = io.connect('http://localhost:4040');
+var output=document.getElementById('output'),
+    handle=document.getElementById('handle'),
+    message=document.getElementById('message'),
+    button=document.getElementById('send'),
+    feedback=document.getElementById('feedback'),
+    num=document.getElementById('num'),
+    list=document.getElementById('list');
+/*
+    //from here
+    var username;
+        var $loginpage = $('.loginpage');
+        var $chatpage = $('.chatpage');
+        var $usernameInput = document.getElementById($('.usernameInput'));
+        
+            username = $usernameInput.val().trim();
+        
+            // If the username is valid
+            
+              $loginPage.fadeOut();
+              $chatPage.show();
+              $loginPage.off('click');
+        
+              // Tell the server your username
+              socket.emit('add user', username);
+            
+          }
+    //to here
+    document.getElementById('handle').innerHTML(username);
+*/
+button.addEventListener('click',function(){
+    socket.emit('chat',{
+        message: message.value,
+        handle: handle.value
+    });
+    message.value="";
+
+});
+message.addEventListener('keypress',function(){
+    socket.emit('typing',handle.value);
+});
+socket.on('chat',function(data){
+    feedback.innerHTML='';
+    output.innerHTML += '<p><strong>' + data.handle + ':</strong>' + data.message + '</p>';
+});
+socket.on('typing',function(data){
+    feedback.innerHTML = '<p><em>' + data + ' is typing..' + '</p></em>';
+});
+socket.on('number',function(data){
+    num.innerHTML = data.description;
+    list.innerHTML= data.Name;
+});

9d42bb811b930c3ee6d1000c358ddf9534cfa450
ronishpakhrin1 <ronishpakhrin@gmail.com>
2019-08-01 20:28:46 +0530

    changed

--- broadcast_chat2/server.js
+++ broadcast_chat2/server.js
@@ -1,22 +1,43 @@
 var http = require('http');
 var express = require('express');
 var socket = require('socket.io');
+var readline = require('readline');
 
 var app = express();
 var server = http.createServer(app);
 var io = socket(server);
 
 app.use(express.static('public'));
-     
+var clients = 0;
+var users=[];
 io.on('connection',function(socket){
-    console.log('connection made');
-
-    socket.on('chat',function(data){
-        io.emit('chat',data);
-        
+    clients++;
+    console.log('connection made by ' + socket.id);
+    users.push(socket.id +'<br/>');
+    io.emit('number',{description: clients + '  online.',
+        Name: users
     });
+   /*//from here
+   socket.on('add user', (username) => {
+    // we store the username in the socket session for this client
+    users.push(username +'<br/>');
+    });
+   //to here*/
+    socket.on('chat',function(data){
+        io.emit('chat',data);   
+    });
+  
     socket.on('typing',function(data){
         socket.broadcast.emit('typing',data);
+    });
+
+    socket.on('disconnect',function(){
+        clients--;
+        io.emit('number',{description: clients + '  online.'});
+        users.splice(1,1);
+        if(clients==0){
+                users.splice(0,1);
+        }
     });
 });
 

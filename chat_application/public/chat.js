fd81c780f963dcbd1cdcdd00aba4ef264262f8b9
ronishpakhrin1 <ronishpakhrin@gmail.com>
2019-08-06 20:12:47 +0530

    Update

--- chat_application/public/chat.js
+++ chat_application/public/chat.js
@@ -1,16 +1,44 @@
+//connecting to the server
 var socket = io.connect('http://localhost:4040');
+
+//dom manipulation
 var output=document.getElementById('output'),
     message=document.getElementById('message'),
     button=document.getElementById('send'),
     feedback=document.getElementById('feedback'),
     num=document.getElementById('num'),
+    createRoom=document.getElementById('create_room'),
+    leaveRoom=document.getElementById('leave_room'),
     list=document.getElementById('list');
-var userName = window.prompt('Enter Your Name');
+
+    //promt for username
+    var userName = window.prompt('Enter Your Name');
     if(userName === ''){
         window.location.reload();
     }
-document.getElementById('handle').innerHTML=userName;
-socket.emit('username',userName);
+
+    document.getElementById('handle').innerHTML=userName;
+    socket.emit('username',userName);
+   
+//chat room
+createRoom.addEventListener('click',function(){
+    let roomName = window.prompt('Enter the room name');
+    if(roomName===''){
+        window.location.reload();
+    }
+    socket.emit('create',roomName);
+});
+
+//leave room
+leaveRoom.addEventListener('click',function(){
+    let roomName = window.prompt('Enter the room name');
+    if(roomName===''){
+        window.location.reload();
+    }
+    socket.emit('leave',{room:roomName,handle:userName});
+});
+
+//send the message
 button.addEventListener('click',function(){
     socket.emit('chat',{
         message: message.value,
@@ -18,9 +46,13 @@ button.addEventListener('click',function(){
     });
     message.value="";
 });
+
+//typing thing
 message.addEventListener('keypress',function(){
     socket.emit('typing',userName);
 });
+
+//receiving from the server
 socket.on('chat',function(data){
     feedback.innerHTML='';
     output.innerHTML += '<p><strong>' + data.handle + ':</strong>' + data.message + '</p>';
@@ -28,15 +60,30 @@ socket.on('chat',function(data){
 
 socket.on('pchat',function(data){
     feedback.innerHTML='';
-    output.innerHTML += '<span class="per"><b>' +data.name +':</b>' + data.message +'<br/>'+ '</span>'
+    output.innerHTML += '<span class="per"><b>'+'>>'+data.name +':</b>' + data.message +'<br/>'+ '</span>';
 });
 
 socket.on('typing',function(data){
     feedback.innerHTML = '<p><em>' + data + ' is typing..' + '</p></em>';
 });
 socket.on('print',function(data){
-    list.innerHTML = data;
+    list.innerHTML = data ;
 });
 socket.on('number',function(data){
     num.innerHTML = data.description;
+});
+socket.on('feed',function(data){
+    feedback.innerHTML='';
+    output.innerHTML +='<span class="feed">'+'--'+data + '  joined'+'</br>'+'</span>';
+});
+socket.on('feed1',function(data){
+    feedback.innerHTML='';
+    output.innerHTML+='<span class="feed">'+'--'+data+ '  left'+'</br>'+'</span>';
+});
+socket.on('event',function(data){
+    output.innerHTML+='<span class="per"><b>'+'^^'+data.handle+' :'+data.message +'</b>'+'<br/>'+'</span>';
+});
+socket.on('eventLeave',function(data){
+    output.innerHTML +='<span class="per"><b>'+'^^'+data+'  :left the room' +':</b>'+'<br/>'+'</span>';
+
 });
--- chat_application/server.js
+++ chat_application/server.js
@@ -1,81 +1,111 @@
-var http = require('http');
-var express = require('express');
-var socket = require('socket.io');
-var readline = require('readline');
-
-var app = express();
-var server = http.createServer(app);
-var io = socket(server);
+//environment setup
+var http = require('http'),
+    express = require('express'),
+    socket = require('socket.io'),
+    app = express(),
+    server = http.createServer(app),
+    io = socket(server),
+    clients = 0,list = [],list1 = [];
 
 app.use(express.static('public'));
-var clients = 0;
-var list=[];
-var list1=[];
+
 //connection
-io.on('connection',function(socket){
+io.on('connection', function (socket) {
     clients++;
     console.log('connection made by ' + socket.id);
-    io.emit('number',{description: clients + '  online.'});
+    io.emit('number', { description: clients + '  online.' });
+
     //set  username
-    socket.on('username',function(data){
-            list.push({
-                id: socket.id,
-                name: data
-            });
-            let len = list.length;
-            len--;
-            list1.push(list[len].name+'<br/>');
-            io.emit('print',list1);
+    socket.on('username', function (data) {
+        list.push({
+            id: socket.id,
+            name: data
         });
-   //send the message
-    socket.on('chat',function(data){
+        let len = list.length;
+        len--;
+        list1.push(list[len].name+'</br>');
+        io.emit('print', list1);
+        io.emit('feed',data);
+    });
+
+    //send the message
+    socket.on('chat', function (data) {
         var msg = data.message.trim();
         console.log(msg);
-        if(msg.substr(0,1) ==='@'){
+        //private
+        if (msg.substr(0, 1) === '@') {
             var ind = msg.indexOf(' ');
-            var uname = msg.substr(1,ind).trim();
+            var uname = msg.substr(1, ind).trim();
             console.log(uname);
-            var msg = msg.substr(ind,msg.length).trim();
+            var msg = msg.substr(ind, msg.length).trim();
             console.log(msg);
             console.log('hello');
-            var id,sender,id2;
-            for(let i=0;i<list.length;i++){
-                if(list[i].name === uname){
-                    id=list[i].id;
+            var id, sender, id2;
+            for (let i = 0; i < list.length; i++) {
+                if (list[i].name === uname) {
+                    id = list[i].id;
                     console.log(id);
                     break;
                 }
-              }
-              for(let i=0;i<list.length;i++){
-                  if(list[i].name === data.handle){
-                      id2=list[i].id;
-                      sender=list[i].name;
-                      console.log(sender);
-                      break;
-                  }
-              }
-              io.to(id2).emit('pchat',{message: msg,name: sender});
-              io.to(id).emit('pchat',{message: msg,name: sender});
-            console.log('outside the loop');
             }
-        else{
-            io.emit('chat',data);   
-        }  
-    });
-    //send the typing thing
-    socket.on('typing',function(data){
-        socket.broadcast.emit('typing',data);
-    });
-   //when disconnected
-    socket.on('disconnect',function(){
-        clients--;
-        io.emit('number',{description: clients + '  online.'});
-        for(let i=0;i<list.length;i++){
-          if(list[i].id === socket.id){
-              list1.splice(i,1);
-          }
+            for (let i = 0; i < list.length; i++) {
+                if (list[i].name === data.handle) {
+                    id2 = list[i].id;
+                    sender = list[i].name;
+                    console.log(sender);
+                    break;
+                }
+            }
+            io.to(id2).emit('pchat', { message: msg, name: sender });
+            io.to(id).emit('pchat', { message: msg, name: sender });
         }
-       io.emit('print',list1);
+        //public
+        else {
+            io.emit('chat', data);
+        }
+    });
+    
+    //chat room
+    socket.on('create',function(room){
+        socket.join(room);
+        console.log('connected to the room '+room);
+        console.log(socket.adapter.rooms);
+        socket.on('chat',function(data){
+                io.sockets.to(room).emit('event',data);
+        });
+       // io.sockets.to(room).emit('event',{message: 'lets party',handle:name});
+    });
+
+    //leave room
+    socket.on('leave',function(data){
+        var room=data.room,
+            name=data.handle;
+        socket.leave(room);
+        console.log(socket.adapter.rooms);
+        console.log(name+' left');
+        io.sockets.to(room).emit('eventLeave',name);
+    });
+
+    //send the typing thing
+    socket.on('typing', function (data) {
+        socket.broadcast.emit('typing', data);
+    });
+
+    //when disconnected
+    socket.on('disconnect', function () {
+        clients--;
+        var left;
+        io.emit('number', { description: clients + '  online.' });
+        for (let i = 0; i < list.length; i++) {
+            if (list[i].id === socket.id) {
+                console.log(list[i].name);
+                left=list[i].name;
+                list1.splice(i,1);
+                console.log('deleted');
+            }
+        }
+        io.emit('print', list1);
+        io.emit('feed1',left);
     });
 });
 

813aa265aa51c5009848b7ff8f06b1fb927061c0
ronishpakhrin1 <ronishpakhrin@gmail.com>
2019-08-14 19:46:23 +0530

    updated

--- mochaTest/test/appTest.js
+++ mochaTest/test/appTest.js
@@ -1,5 +1,6 @@
 const assert = require('chai').assert;
 const app = require('../app');
+const http = require('http');
 
 checkStringResult=app.checkString();
 checkNumResult=app.checkNum(5,5);
@@ -14,12 +15,40 @@ describe('app',function(){
         });
     });
 
-    describe('checkSum',function(){
+    describe('checkSum()',function(){
         it('check if the sum is above 5',function(){
             assert.isAbove(checkNumResult,5);
         });
         it('check if its a number',function(){
-            assert.typeOf(checkNumResult,'Number');
+            assert.typeOf(checkNumResult,'number');
         });
     })
+
+    describe('checkCallback()',function(){
+        it('check if callback is working',function(done){
+                    http.get('http://www.google.com',function(res){
+                        assert.equal(res.statusCode,'200');
+                        done();
+                    });
+                  
+        });
+    });
+
+    describe('checkPromise',function(){
+        it('check if promise is working',function(){
+            var promise1=new Promise(function(resolve,reject){
+                
+                http.get('http://www.google.com',function(res){
+                    resolve(res.statusCode);
+                });
+                
+
+            });
+            promise1.then(function(value){
+                assert.equal(value,850);
+            }).catch(function(error){
+                throw new Error(error);
+            });
+        })
+    });
 });
